0795db92c71ff1b471f4cd9d39435e6b04d1cf94
Angie Tran <tranangiekim@gmail.com>
2023-06-20 22:50:12 -0600

    created CatList and Cat components

---
+++ src/components/Cat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cat = () => {
+    return (
+        <li>
+        <h3>Cat Name</h3>
+        <button>Pet Cat</button>
+    </li>
+    );
+}
+export default Cat;
---
+++ src/components/CatList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cat from './Cat';
+
+const CatList = () => {
+    return (
+        <section>
+            <h2>Cat list</h2>
+            <ul>
+                <Cat />
+            </ul>
+        </section>
+    )
+}
+export default CatList;
